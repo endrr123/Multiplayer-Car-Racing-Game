cf1851082ebed17f2df686ca323987c32d8d3a73
endrr123 <vatsal.tewari123@gmail.com>
2021-09-08 19:07:45 +0530

    Added images to sprites and background

--- js/Game.js
+++ js/Game.js
@@ -32,10 +32,14 @@ class Game{
             form.display();
             
         }
-        car1 = createSprite(100,200);
-        car2 = createSprite(300,200);
-        car3 = createSprite(500,200);
-        car4 = createSprite(700,200);
+        car1 = createSprite(380,200);
+        car1.addImage("car1",car1Image)
+        car2 = createSprite(580,200);
+        car2.addImage("car2",car2Image)
+        car3 = createSprite(780,200);
+        car3.addImage("car3",car3Image)
+        car4 = createSprite(880,200);
+        car4.addImage("car4",car4Image)
         cars = [car1 , car2 , car3 , car4];
 
     }
@@ -45,11 +49,13 @@ class Game{
         textSize(30)
         text("Game Starts", 120, 100);
         Player.getPlayerInfo();
+
         if(allPlayers !== undefined){
+            image(trackImage, 0, -displayHeight*4, displayWidth, displayHeight*5)
 
             //var displayPosition = 130;
             var index = 0;
-            var x = 0;
+            var x = 200;
             var y = 0;
             
             
@@ -76,6 +82,7 @@ class Game{
                 // text(allPlayers[plr].name + " : " + allPlayers[plr].distance, 120, displayPosition);
             }
             }
+    
 
             
 
@@ -83,8 +90,15 @@ class Game{
                 player.distance = player.distance + 50;
                 player.update();
             }
+            if(player.distance >= 4150){
+                gameState = 2;
+            }
 
             drawSprites()
+        }
+
+        end(){
+            console.log("game ended")
         }
 
 
--- sketch.js
+++ sketch.js
@@ -5,9 +5,19 @@ var player;
 var gameState = 0;
 var playerCount;
 var allPlayers;
-var car1,car2,car3,car4;
+var car1,car2,car3,car4,  car1Image, car2Image, car3Image, car4Image
 var cars;
+var groundImage, trackImage;
 
+function preload(){
+    car1Image = loadImage("images/car1.png")
+    car2Image = loadImage("images/car2.png")
+    car3Image = loadImage("images/car3.png")
+    car4Image = loadImage("images/car4.png")
+    trackImage = loadImage("images/track.jpg")
+    groundImage = loadImage("images/ground.png")
+    
+}
 function setup(){
     createCanvas(displayWidth-30,displayHeight-40);
     database = firebase.database()
@@ -19,6 +29,7 @@ function setup(){
 
 function draw(){
     background("white");
+
     if(playerCount === 4){
         game.update(1)
     }
@@ -26,6 +37,9 @@ function draw(){
     if(gameState === 1){
         clear()
         game.play()
+    }
+    if(gameState === 2){
+        game.end()
     }
 }
 
